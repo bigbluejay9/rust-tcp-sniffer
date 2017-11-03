c39d1ca486ccc9b2427778c30b3c0b1989b98884
Laza Upatising <lazau@lazau.com>
2017-11-02 19:55:50 -0700

    Sniff complete.

--- src/main.rs
+++ src/main.rs
@@ -26,19 +26,19 @@ fn start_sniffing(lis: &SocketAddr, fwd: &SocketAddr) -> Result<(), std::io::Err
     // SERVER + 1.
     let mut token_counter = 1;
     // Map k: My Token -> (My TcpStream, My Write Buffer, Fwd Stream Token)
-    let mut event_map: HashMap<Token, (TcpStream, Vec<u8>, &Token)> = HashMap::new();
+    let mut event_map: HashMap<Token, (TcpStream, bool, Vec<u8>, mio::Ready, Token)> =
+        HashMap::new();
 
     poll.register(&listener, SERVER, Ready::readable(), PollOpt::edge())
         .unwrap();
 
     let mut events = Events::with_capacity(1024);
     loop {
-        debug!("poll");
+        debug!("poll: {:?}", event_map);
         poll.poll(&mut events, None).unwrap();
-        debug!("poll return");
 
         for event in events.iter() {
-          debug!("Iterating through events. Current event: {:?}.", event);
+            debug!("Iterating through events. Current event: {:?}.", event);
             match event.token() {
                 SERVER => {
                     let conn = listener.accept()?;
@@ -60,41 +60,64 @@ fn handle_new_connection(
     (c, _remote): (TcpStream, SocketAddr),
     poll: &Poll,
     token_counter: usize,
-    event_map: &mut HashMap<Token, (TcpStream, Vec<u8>, &Token)>,
+    event_map: &mut HashMap<Token, (TcpStream, bool, Vec<u8>, mio::Ready, Token)>,
     fwd: &SocketAddr,
 ) -> Result<(), std::io::Error> {
     debug!("Accepted connection: {:?}.", c);
     debug!("Dialing: {:?}.", fwd);
-    let (upstreamer, downstreamer) = (Token(token_counter),
-        Token(token_counter+1));
+    let (upstreamer, downstreamer) = (Token(token_counter), Token(token_counter + 1));
     let conn = match TcpStream::connect(fwd) {
-      Ok(c) => c,
+        Ok(c) => c,
         Err(e) => {
-          warn!("Can't connect to forward address: {}.", e.to_string());
-          return Ok(());
+            warn!("Can't connect to forward address: {}.", e.to_string());
+            return Ok(());
         }
     };
 
     debug!(
         "Registering upstreamer & downstreamer: \
         between {:?} and {:?}, tokens {:?} and {:?}.",
-        c, conn, &upstreamer, &downstreamer);
-    // Insert downstream -> upstream.
-    event_map.insert(upstreamer, (&mut c, Vec::new(), &downstreamer));
-    // Insert upstream -> downstream.
-    event_map.insert(downstreamer, (conn, Vec::new(), &upstreamer));
+        c,
+        conn,
+        &upstreamer,
+        &downstreamer
+    );
+    let upstream_monitor = Ready::readable();
     poll.register(
         &c,
-        Token(token_counter),
-        Ready::readable(),
+        upstreamer,
+        upstream_monitor,
         PollOpt::edge(),
     )?;
+
+    let downstream_monitor = Ready::empty();
+    poll.register(
+        &conn,
+        downstreamer,
+        downstream_monitor,
+        PollOpt::edge(),
+    )?;
+    // Insert downstream -> upstream.
+    event_map.insert(upstreamer, (
+        c,
+        false,
+        Vec::new(),
+        upstream_monitor,
+        downstreamer,
+    ));
+    // Insert upstream -> downstream.
+    event_map.insert(downstreamer, (
+        conn,
+        false,
+        Vec::new(),
+        downstream_monitor,
+        upstreamer,
+    ));
+
     Ok(())
 }
 
-fn maybe_write(
-    dest: &mut TcpStream,
-    data: &mut Vec<u8>) -> Result<usize, std::io::Error> {
+fn maybe_write(dest: &mut TcpStream, data: &mut Vec<u8>) -> Result<usize, std::io::Error> {
     let mut written_bytes = Vec::with_capacity(data.len());
     let mut count = 0;
     loop {
@@ -112,8 +135,8 @@ fn maybe_write(
             Err(e) => return Err(e),
         }
     }
-    debug!(
-        "Written {} bytes: {}.",
+    warn!(
+        "Written {} bytes: {:?}.",
         count,
         String::from_utf8(written_bytes).unwrap()
     );
@@ -122,71 +145,121 @@ fn maybe_write(
 
 fn maybe_read(
     poll: &Poll,
-    read: &mut TcpStream,
-    write_token: &Token,
-    write: &mut TcpStream,
-    write_buf: &mut Vec<u8>) -> Result<usize, std::io::Error> {
-  let mut buf = [0; 256];
-  let mut total_read = Vec::new();
-  loop {
-    match read.read(&mut buf) {
-      Ok(b) => {
-        total_read.extend_from_slice(&buf[0..b]);
-      }
-      Err(ref e) if e.kind() == WouldBlock => {
-        break;
-      }
-      Err(e) => return Err(e),
+    e: &Token,
+    event_map: &mut HashMap<Token, (TcpStream, bool, Vec<u8>, mio::Ready, Token)>,
+) -> Result<usize, std::io::Error> {
+    debug!("Maybe reading {:?}", e);
+    let mut total_read = Vec::new();
+    let write_token: Token;
+    {
+        // Mut borrow scope for read TcpStream.
+        let &mut (ref mut read, _, _, _, wt) = event_map.get_mut(e).expect(&format!(
+            "event map lacking read entry {:?}",
+            e
+        ));
+        assert!(wt != *e);
+        write_token = wt;
+        let mut buf = [0; 256];
+        loop {
+            match read.read(&mut buf) {
+                Ok(b) => {
+                    if b == 0 {
+                        if total_read.len() == 0 {
+                            return Err(std::io::Error::new(std::io::ErrorKind::BrokenPipe, "EOF"));
+                        }
+                        break;
+                    }
+                    total_read.extend_from_slice(&buf[0..b])
+                }
+                Err(ref e) if e.kind() == WouldBlock => {
+                    break;
+                }
+                Err(e) => return Err(e),
+            }
+        }
+        warn!(
+            "Read {} bytes: {:?}.",
+            total_read.len(),
+            String::from_utf8(total_read.clone()).unwrap()
+        );
     }
-  }
-  debug!("Read {} bytes: {}.", total_read.len(),
-      String::from_utf8(total_read.clone()).unwrap());
-  let len = total_read.len();
-  if len > 0 {
-    write_buf.extend(total_read);
-    poll.reregister(write, *write_token, Ready::readable() | Ready::writable(), PollOpt::edge())?;
-  }
-  Ok(len)
+
+    // If we read something, put it on the destination TcpStream and register
+    // kevent.
+    let len = total_read.len();
+    if len > 0 {
+        let &mut (ref write, _, ref mut write_buf, monitor, _) =
+            event_map.get_mut(&write_token).expect(&format!(
+                "event map lacking write buf dest entry {:?}",
+                &write_token
+            ));
+        write_buf.extend(total_read);
+        if write_buf.len() > 0 {
+            poll.reregister(
+                write,
+                write_token,
+                monitor | Ready::writable(),
+                PollOpt::edge(),
+            )?;
+        }
+    }
+    Ok(len)
 }
 
 fn handle_event(
     poll: &Poll,
     e: &Token,
-    event_map: &mut HashMap<Token, (TcpStream, Vec<u8>, &Token)>,
+    event_map: &mut HashMap<Token, (TcpStream, bool, Vec<u8>, mio::Ready, Token)>,
 ) -> Result<(), std::io::Error> {
     debug!("Handling event: {:?}", e);
-    let read_clone;
-    {
-    let &mut (ref mut read, ref mut data, ref write) = event_map.get_mut(e).expect(&format!(
-        "event map lacking entry {:?}",
-        e
-    ));
-    read_clone = &mut read.try_clone()?;
+    match maybe_read(poll, e, event_map) {
+        Err(ref err) if err.kind() == std::io::ErrorKind::BrokenPipe => {
+            // Shutdown(write) other side.
+            let remove_self;
+            {
+                let &(_, already_shutdown, _, _, other) = event_map.get(e).unwrap();
+                remove_self = already_shutdown;
+                match event_map.get_mut(&other) {
+                    Some(&mut (ref mut write, ref mut shutdown, _, _, _)) => {
+                        *shutdown = true;
+                        write.shutdown(std::net::Shutdown::Write)?;
+                    }
+                    None => debug!("Other pipe already gone."),
+                };
+            }
+            // If out write's already been shutdown, then close.
+            if remove_self {
+                {
+                    let &(ref c, _, _, _, _) = event_map.get(e).unwrap();
+                    poll.deregister(c)?;
+                }
+                event_map.remove(e);
+            }
+            return Ok(());
+        }
+        _ => {}
     }
-    let &mut (_, ref mut data, ref write) = event_map.get_mut(e).expect(&format!(
-        "event map lacking entry {:?}",
-        e
-    ));
     {
-      let read_clone;
-      let write_token;
-      {
-        let &mut (ref mut read, _, ref wt) =
-          event_map.get_mut(e).expect(&format!( "event map lacking read entry {:?}", e));
-        read_clone = &mut read.try_clone()?;
-        write_token = &wt;
-      }
-      let &mut (ref mut write, ref mut write_buffer, _) =
-          event_map.get_mut(write_token).expect(&format!( "event map lacking write entry {:?}", write_token));
-      maybe_read(poll, read_clone, write_token, write, write_buffer);
-    }
-    maybe_write(read_clone, data)?;
+        // Mutable borrow of TcpStream and pending write buf identified by e.
+        let &mut (ref mut stream, shutdown, ref mut buf, _, _) =
+            event_map.get_mut(e).expect(&format!(
+                "event map lacking write entry {:?}",
+                e
+            ));
+        maybe_write(stream, buf)?;
+        if shutdown && buf.len() > 0 {
+            warn!(
+                "Discarding bytes: {:?}",
+                String::from_utf8(buf.clone()).unwrap()
+            );
+        }
 
-    let mut monitor = Ready::readable();
-    if data.len() > 0 {
-      monitor |= Ready::writable();
+        let monitor = match buf.len() {
+            0 => Ready::readable(),
+            _ => Ready::readable() | Ready::writable(),
+        };
+        poll.reregister(stream, *e, monitor, PollOpt::edge())?;
     }
-    poll.reregister(read_clone, *e, monitor, PollOpt::edge())?;
     Ok(())
 }
 
